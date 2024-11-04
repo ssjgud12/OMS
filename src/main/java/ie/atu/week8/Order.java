1319a37e8c31f89d0471cc8dce7f217a5cc5056a
ALVINOLAYEMI-STUDENT <G00428825@atu.ie>
2024-11-04 09:39:20 +0000

    Created constructor and initialsed strings

---
+++ src/main/java/ie/atu/week8/Order.java
@@ -0,0 +1,26 @@
+package ie.atu.week8;
+
+import com.sun.jdi.connect.Connector;
+
+public class Order {
+
+    public Order()
+    {
+
+    }
+
+    public static void main(String[] args) {
+
+
+        String orderId;
+        String customername;
+        String productName;
+        int quantity;
+
+        customername = "Alvin";
+        orderId = "1111";
+        productName = "shirts";
+        quantity = 1000;
+        
+    }
+}
