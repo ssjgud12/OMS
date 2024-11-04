5bcda82d0232fc7c50592d9ebda4836edac8bd35
ALVINOLAYEMI-STUDENT <G00428825@atu.ie>
2024-11-04 10:00:46 +0000

    Started menu interface and ArrayList

---
+++ src/main/java/ie/atu/week8/Menu.java
@@ -0,0 +1,12 @@
+package ie.atu.week8;
+
+public class Menu
+{
+    public static void main(String[] args)
+    {
+      System.out.println("Add Order\n\n");
+        System.out.println("Update Order\n\n");
+        System.out.println("Display All orders\n\n");
+        System.out.println("Exit\n\n");
+    }
+}
--- src/main/java/ie/atu/week8/Order.java
+++ src/main/java/ie/atu/week8/Order.java
@@ -15,12 +15,34 @@ public class Order {
         String orderId;
         String customername;
         String productName;
-        int quantity;
+        int[] quantity;
 
         customername = "Alvin";
         orderId = "1111";
         productName = "shirts";
-        quantity = 1000;
-        
+
+
+
+        quantity = new int[10];
+
+        quantity[0] = 1;
+        quantity[1] = 2;
+        quantity[2] = 3;
+        quantity[3] = 4;
+        quantity[4] = 5;
+        quantity[5] = 6;
+        quantity[6] = 7;
+        quantity[7] = 8;
+        quantity[8] = 9;
+        quantity[9] = 10;
+
+        System.out.println("Order objects");
+        for (int i = 0; i < 1;)
+        {
+            System.out.println("",quantity);
+                    i++;
+        }
+
+
     }
 }
